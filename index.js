d8c12df52faf125b4d0e51bbf9eea6929eba41bd
Ali Hamza <alihamza022bb@gmail.com>
2023-10-01 17:46:46 +0500

    made

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const btnMenu = document.querySelector('.menu')
 const list = document.querySelector('.list')
+const smallList = document.querySelector('.small-list')
+const sideList = document.querySelector('.sidebar')
+const listItem = document.querySelector('.list-item')
 
 btnMenu.addEventListener('click', function() {
-    if(list.style.display === 'block' || list.style.display === '')
+    smallList.style.transition = 'all 0.4s ease-in-out'
+    list.style.transition = 'all 0.4s ease-in-out'
+    if(list.style.display === 'block' || list.style.display === '') {
+        smallList.style.display = 'block'
         list.style.display = 'none'
-    else
+        sideList.style.width = '10%'
+        btnMenu.style.marginLeft = '1rem'
+    } else {
+        smallList.style.display = 'none'
         list.style.display = 'block'
-
-    const bar1 = document.querySelector('.bar1')
-    const bar2 = document.querySelector('.bar2')
-    const bar3 = document.querySelector('.bar3')
-
-    bar1.style.cssText = 'transform: translate(45deg)'
-    bar2.style.cssText = 'display: none'
+        sideList.style.width = '15%'
+        btnMenu.style.marginLeft = '2rem'
+        // listItem.style.margin = '0.3rem'
+    }
 })
